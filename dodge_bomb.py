b05903f9e22284ea2444e7ffab8cd601a28ae12e
hide-hide <c0c24049fb@edu.teu.ac.jp>
2025-09-30 14:43:07 +0900

    練習4

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -45,6 +45,8 @@ def main():
             if event.type == pg.QUIT: 
                 return
         screen.blit(bg_img, [0, 0]) 
+        if kk_rct.colliderect(bb_rct):#こうかとんと爆弾の衝突判定
+            return# ゲームオーバー
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
