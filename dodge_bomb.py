ef8b4694378f5780f526a0584bba8130e3ebe4d2
hide-hide <c0c24049fb@edu.teu.ac.jp>
2025-09-30 14:12:18 +0900

    練習2

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,4 +1,5 @@
 import os
+import random
 import sys
 import pygame as pg
 
@@ -22,6 +23,13 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+    bb_img = pg.Surface((20,20))#空のサーフェイス
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
+    bb_img.set_colorkey((0, 0, 0)) #四隅を削除
+    bb_rct = bb_img.get_rect()
+    bb_rct.centerx = random.randint(0, WIDTH)#爆弾横座標
+    bb_rct.centery = random.randint(0, HEIGHT)#爆弾縦座標
+    vx, vy = +5, +5
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -46,6 +54,8 @@ def main():
          #   sum_mv[0] += 5
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
+        bb_rct.move_ip(vx, vy)
+        screen.blit(bb_img, bb_rct) #爆弾描画
         pg.display.update()
         tmr += 1
         clock.tick(50)
